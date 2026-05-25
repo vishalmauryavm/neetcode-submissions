fa9b65a975fd88565e28c0f612f253f8529bd949
vishalmauryavm <vishalmauryavm00@gmail.com>
2026-05-25 17:22:23 +0530

    Add: length-of-last-word - submission-2

---
+++ Data Structures & Algorithms/length-of-last-word/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        int count =0;
+        if (s.length()==1)
+        return 1;
+
+        for(int i= s.length()-1;i>1;i--){
+            if (!Character.isWhitespace(s.charAt(i))){
+                count++;
+            }
+            else
+            break;
+        }
+        return count;
+    }
+}

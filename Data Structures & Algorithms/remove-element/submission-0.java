60b933f2661db143900fff13f8f0d7415fc8b7fa
vishalmauryavm <vishalmauryavm00@gmail.com>
2026-05-29 12:51:28 +0530

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int n = nums.length;
+        int count=n;
+        for (int i=0;i<n;i++){
+            if(nums[i]==val){
+                count--;
+                nums[i]=Integer.MAX_VALUE;
+            }
+        }
+        Arrays.sort(nums);
+        return count;
+        
+    }
+}
